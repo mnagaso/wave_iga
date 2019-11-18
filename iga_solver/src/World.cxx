fb057cae46f0ee46df5f9a438d9a89ccfeb4db4e
Masaru Nagaso <mnsaru22@gmail.com>
2019-11-18 07:21:42 +0100

    add template files

---
+++ iga_solver/src/World.cxx
@@ -0,0 +1,31 @@
+///
+/// @file    World.cxx
+/// @author  Barthelemy von Haller
+///
+
+#include "iga_solver/World.h"
+#include "mesh_converter/World.h"
+
+#include <iostream>
+
+namespace ProjectTemplate {
+namespace iga_solver {
+
+void World::greet()
+{
+  std::cout << "iga_solver world!!" << std::endl;
+
+  ProjectTemplate::mesh_converter::World projAWorld;
+  projAWorld.greet();
+}
+
+int World::returnsN(int n)
+{
+
+  /// \todo This is how you can markup a todo in your code that will show up in the documentation of your project.
+  /// \bug This is how you annotate a bug in your code.
+  return n;
+}
+
+} // namespace iga_solver
+} // namespace ProjectTemplate
---
+++ mesh_converter/cmake/mesh_converterConfig.cmake
@@ -0,0 +1,1 @@
+include("${CMAKE_CURRENT_LIST_DIR}/mesh_converterTargets.cmake")
